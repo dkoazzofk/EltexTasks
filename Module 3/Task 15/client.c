1b1116bb6f0215064a8d40642e8b8aabc658f22a
dkoazzofk <zotovan77@mail.ru>
2025-05-16 00:49:15 +0700

    add task 15

---
+++ Module 3/Task 15/client.c
@@ -0,0 +1,65 @@
+#include <arpa/inet.h>
+#include <errno.h>
+#include <netinet/in.h>
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[]){
+    int sockfd;
+    pthread_t tid;
+    struct sockaddr_in servaddr;
+
+    if((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0){
+        perror("socket");
+        exit(EXIT_FAILURE);
+    }
+    memset(&servaddr, 0, sizeof(servaddr));
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_port = htons(atoi(argv[2]));
+    if(inet_pton(AF_INET, argv[1], &servaddr.sin_addr) < 1){
+        perror("wrong IP");
+        close(sockfd);
+        exit(EXIT_FAILURE);
+    }
+    if(connect(sockfd,(struct sockaddr *)&servaddr, sizeof(servaddr)) < 0){
+        perror("connect");
+        close(sockfd);
+        exit(EXIT_FAILURE);
+    }
+    if(pthread_create(&tid, NULL, recv_message, NULL) < 0){
+        perror("pthread_create");
+        close(sockfd);
+    }
+
+    char message[1024];
+    while(1){
+        if(fgets(message, 1024, stdin) != NULL){
+            if(message[strlen(message) - 1] == '\n'){
+                message[strlen(message) - 1] == '\0';
+            }
+        }
+        if(!strcmp(message, "q")){
+            printf("Exit...");
+            if(pthread_cancel(tid) != 0){
+                perror("pthread_cancel");
+                exit(EXIT_FAILURE);
+            }
+            break;
+        }
+        if(send(sockfd, message, strlen(message) + 1, 0) < 0){
+            perror("send");
+            exit(EXIT_FAILURE);
+        }
+    }
+    if(pthread_join(tid, NULL) != 0){
+        perror("pthread_join");
+        exit(EXIT_FAILURE);
+    }
+    close(sockfd);
+    eixt(EXIT_SUCCESS);
+}
---
+++ Module 3/Task 15/server.c
@@ -0,0 +1,176 @@
+#include <arpa/inet.h>
+#include <errno.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/time.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+
+void calculate(int);
+
+double sum(double a, double b) {
+    return a + b;
+}
+
+double division(double a, double b) {
+    if (b == 0)
+        return 0;
+    return a / b;
+}
+
+double multiplication(double a, double b) {
+    return a * b;
+}
+
+double subtraction(double a, double b) {
+    return a - b;
+}
+
+typedef struct{
+    const char *name;
+    double (*func)(double, double);
+} Operation;
+
+void error(const char *msg){
+    perror(msg);
+    exit(1);
+}
+
+int main(int argc, char *argv[]){
+    int sockfd, newsockfd;
+    struct sockaddr_in servaddr, cliaddr;
+
+    if((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0){
+        perror("socket create");
+        exit(EXIT_FAILURE);
+    }
+
+    memset(&servaddr, 0, sizeof(servaddr));
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_port = htons(atoi(argv[2]));
+    servaddr.sin_addr.s_addr = INADDR_ANY;
+
+    if(bind(sockfd, (struct sockaddr*)&servaddr, sizeof(servaddr))){
+        perror("bind failed");
+        close(sockfd);
+        exit(EXIT_FAILURE);
+    }
+    if(listen(sockfd, 5)){
+        perror("listen failed");
+        close(sockfd);
+        exit(EXIT_FAILURE);
+    }
+    fd_set fds;
+    int sel_fds;
+    struct timeval delay;
+    FD_ZERO(&fds);
+    FD_SET(sockfd, &fds);
+    int max_fd = sockfd;
+    socklen_t clilen = sizeof(cliaddr);
+    while(1){
+        delay.tv_sec = 60;
+        delay.tv_usec = 0;
+        if((sel_fds = select(max_fd + 1, &fds, 0, 0, &delay)) < 0){
+            perror("select failed");
+            close(sockfd);
+            exit(EXIT_FAILURE);
+        }
+        if(sel_fds == 0) break;
+        if((newsockfd = accept(sockfd, (struct sockaddr*)&cliaddr, &clilen)) < 0){
+            perror("accept failed");
+            close(sockfd);
+            exit(EXIT_FAILURE);
+        }
+        printf("IP %s connected", inet_ntoa(cliaddr.sin_addr));
+        pid = fork();
+        if(pid < 0){
+            perror("fork");
+            close(sockfd);
+            close(newsockfd);
+            exit(EXIT_FAILURE);
+        }
+        if(pid == 0){
+            close(sockfd);
+            calculate(newsockfd);
+            close(newsockfd);
+            exit(EXIT_FAILURE);
+        } else{
+            close(newsockfd);
+        }
+        close(sockfd);
+        exit(EXIT_FAILURE);
+    }
+
+    return 0;
+}
+
+void calculate(int newsockfd) {
+    Operation operation[] = {
+        {"+", sum},
+        {"/", division},
+        {"*", multiplication},
+        {"-", subtraction},
+    };
+    double a, b, result;
+    int res;
+    char temp[2];
+    char buff[1024];
+    for (;;) {
+        strcpy(buff, "Enter a +-*/ b\n");
+        sleep(1);
+        if (write(newsockfd, buff, strlen(buff) + 1) < 0) {
+            perror("write");
+            close(newsockfd);
+            exit(EXIT_FAILURE);
+        }
+
+        if ((res = read(newsockfd, buff, sizeof(buff))) < 1) {
+            if (res == 0) {
+                return;
+            }
+            perror("read");
+            close(newsockfd);
+            exit(EXIT_FAILURE);
+        }
+
+        if (sscanf(buff, "%lf %s %lf", &a, temp, &b) < 3) {
+            strcpy(buff, "Incorrect data\n");
+            if (write(newsockfd, buff, strlen(buff) + 1) < 0) {
+                perror("write");
+                close(newsockfd);
+                exit(EXIT_FAILURE);
+            }
+            continue;
+        }
+        int i = 0;
+        while (i < sizeof(operation) / sizeof(operation[0])) {
+            if (strcmp(temp, operation[i].name) == 0) {
+                result = operation[i].func(a, b);
+                break;
+            }
+            i++;
+        }
+
+        if (result == 0 && strcmp(operation[i].name, "/") == 0) {
+            strcpy(buff, "You can't divide by zero.\n");
+            if (write(newsockfd, buff, strlen(buff) + 1) < 0) {
+                perror("write");
+                close(newsockfd);
+                exit(EXIT_FAILURE);
+            }
+            continue;
+        }
+        snprintf(buff, sizeof(buff), "%.2lf", result);
+        if (write(newsockfd, buff, strlen(buff) + 1) < 0) {
+            perror("write");
+            close(newsockfd);
+            exit(EXIT_FAILURE);
+        }
+    }
+}
